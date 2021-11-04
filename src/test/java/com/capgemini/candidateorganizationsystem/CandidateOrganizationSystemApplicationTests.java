1b75398c8e734999d878b6aca0108bc3d87e9153
Younes OUFRID <younes.oufrid@hightech.edu>
2021-11-04 23:56:21 +0100

    🎉 Creating new SONIC spring boot backend project
    🎉Initializing a new spring boot startup project

---
+++ src/test/java/com/capgemini/candidateorganizationsystem/CandidateOrganizationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.candidateorganizationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CandidateOrganizationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
