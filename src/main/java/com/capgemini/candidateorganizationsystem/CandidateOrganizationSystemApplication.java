246017a2d8a58adc6e2e7b514686e590de9f2a7a
Younes OUFRID <younes.oufrid@hightech.edu>
2021-11-05 00:31:39 +0100

    🎉 Creating new SONIC spring boot backend project
    🎉Initializing a new spring boot startup project

---
+++ src/main/java/com/capgemini/candidateorganizationsystem/CandidateOrganizationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.candidateorganizationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CandidateOrganizationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CandidateOrganizationSystemApplication.class, args);
+	}
+
+}
