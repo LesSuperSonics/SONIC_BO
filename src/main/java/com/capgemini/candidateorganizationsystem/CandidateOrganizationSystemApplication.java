dc55b5bf5187400b7fabc273e2a9206828078026
Younes OUFRID <younes.oufrid@hightech.edu>
2021-11-18 00:44:33 +0100

    🔨 disabling spring security autoconfiguration momentarily

--- src/main/java/com/capgemini/candidateorganizationsystem/CandidateOrganizationSystemApplication.java
+++ src/main/java/com/capgemini/candidateorganizationsystem/CandidateOrganizationSystemApplication.java
@@ -2,8 +2,9 @@ package com.capgemini.candidateorganizationsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class CandidateOrganizationSystemApplication {
 
 	public static void main(String[] args) {
