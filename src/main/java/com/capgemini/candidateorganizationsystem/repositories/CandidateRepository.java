74eff8298eccd320dac9d4bb9fada5f2f787fba2
Younes OUFRID <younes.oufrid@hightech.edu>
2021-11-24 01:02:41 +0100

    ⚡️ The magical Candidate search solution specs
    ⚡️ The magical Candidate search solution specs

--- src/main/java/com/capgemini/candidateorganizationsystem/repositories/CandidateRepository.java
+++ src/main/java/com/capgemini/candidateorganizationsystem/repositories/CandidateRepository.java
@@ -2,6 +2,7 @@ package com.capgemini.candidateorganizationsystem.repositories;
 
 import com.capgemini.candidateorganizationsystem.entities.CandidateEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface CandidateRepository extends JpaRepository<CandidateEntity, Long> {
+public interface CandidateRepository extends JpaRepository<CandidateEntity, Long>, JpaSpecificationExecutor<CandidateEntity> {
 }
