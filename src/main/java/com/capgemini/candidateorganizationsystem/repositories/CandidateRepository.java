8bc5726144726674c1effce9cc591bfd35e6d92c
Younes OUFRID <younes.oufrid@hightech.edu>
2021-12-06 22:21:51 +0100

    👷 Adding JPQL Queries to get fullstacks , tesings and salesforces
    👷 Adding JPQL Queries to get fullstacks , tesings and salesforces

--- src/main/java/com/capgemini/candidateorganizationsystem/repositories/CandidateRepository.java
+++ src/main/java/com/capgemini/candidateorganizationsystem/repositories/CandidateRepository.java
@@ -4,9 +4,25 @@ import com.capgemini.candidateorganizationsystem.entities.CandidateEntity;
 import com.capgemini.candidateorganizationsystem.entities.CandidateStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface CandidateRepository extends JpaRepository<CandidateEntity, Long>, JpaSpecificationExecutor<CandidateEntity> {
+
     public List<CandidateEntity> findByStatus(CandidateStatus status);
+
+    @Query(value = "SELECT * FROM candidates c WHERE c.profile = 'FullStack' order by c.received_date",
+            nativeQuery = true)
+    List<CandidateEntity> findAllFullStackCandidatesNative();
+
+    @Query(value = "SELECT * FROM candidates c WHERE c.profile = 'Testing' order by c.received_date",
+            nativeQuery = true)
+    List<CandidateEntity> findAllTestingCandidatesNative();
+
+    @Query(value = "SELECT * FROM candidates c WHERE c.profile = 'SalesForce' order by c.received_date",
+            nativeQuery = true)
+    List<CandidateEntity> findAllSalesForceCandidatesNative();
+
 }
